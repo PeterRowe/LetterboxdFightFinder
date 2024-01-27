c419d5461b07096f3fb9ce07abf491fb4424bbd0
Peter Rowe <peterrowe97@gmail.com>
2024-01-27 22:24:15 +0000

    #18 Creating top-five-differnces-endpoint

--- app/app.py
+++ app/app.py
@@ -1,6 +1,14 @@
+from fastapi import FastAPI
 
+from app.letterboxd_scraper import LetterboxdScraper
+from app.ratings_analytics import RatingsAnalytics
 
-class App(object):
-    """
-    The app! Time to fight
-    """
+app = FastAPI()
+letterboxd_scraper = LetterboxdScraper()
+ratings_analytics = RatingsAnalytics()
+
+@app.get('/top-five-differences/{username}')
+def get_top_five_differences(username: str):
+    ratings = letterboxd_scraper.get_user_and_followers_reviews(username=username)
+    differences = ratings_analytics.get_differences_in_ratings(ratings)
+    return differences[0:5]

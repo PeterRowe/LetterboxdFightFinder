39eb71bdf053d59181422237c2998467d1bf7efa
Peter Rowe <peterrowe97@gmail.com>
2024-02-13 20:22:34 +0000

    #20 Creating basic UI

--- app/app.py
+++ app/app.py
@@ -11,13 +11,17 @@ ratings_analytics = RatingsAnalytics()
 
 templates = Jinja2Templates(directory="templates")
 
-@app.get('/top-five-differences/{username}')
-def get_top_five_differences(username: str):
+@app.get('/top-five-differences/', response_class=HTMLResponse)
+def get_top_five_differences(request: Request, username: str):
     ratings = letterboxd_scraper.get_user_and_followers_reviews(username=username)
     differences = ratings_analytics.get_differences_in_ratings(ratings)
-    return differences[0:5]
+    return templates.TemplateResponse(
+        "results.html",
+        {"request": request, "results": differences}
+        )
+
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
     # always good manners to return back the request
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse("index.html", {"request": request}) # Why does not passing request break?

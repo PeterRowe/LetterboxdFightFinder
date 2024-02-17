d9e21585e504daca45b8b54ac2bea3290ab692a7
Peter Rowe <peterrowe97@gmail.com>
2024-02-17 17:20:42 +0000

    #25 Reformatting differences output

--- app/app.py
+++ app/app.py
@@ -11,4 +11,4 @@ ratings_analytics = RatingsAnalytics()
 def get_top_five_differences(username: str):
     ratings = letterboxd_scraper.get_user_and_followers_reviews(username=username)
     differences = ratings_analytics.get_differences_in_ratings(ratings)
-    return differences[0:5]
+    return differences.iloc[0:5]

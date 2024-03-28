ccef721e3fa11aade819b55e42b682011f8ff66d
Peter Rowe <peterrowe97@gmail.com>
2024-03-28 00:10:38 +0000

    # Ensuring both results displays appear at the same time

--- app/app.py
+++ app/app.py
@@ -17,29 +17,62 @@ tmdb_api_handler = TMDBAPIHandler()
 
 templates = Jinja2Templates(directory="templates")
 
-@app.get('/top-five-mutuals-differences/', response_class=HTMLResponse)
-def get_top_five_mutuals_differences(request: Request, username: str, page: int = 0):
+@app.get('/initial-mutual-and-community-differences/', response_class=HTMLResponse)
+def get_initial_mutual_and_community_differences(request: Request, username: str):
 
-    # API is called multiple times during infinite scroll so global variable are used to store values
+    # Caches values for other endpoints to use
     global user_and_mutual_ratings
     global user_and_mutual_differences
+    global user_and_community_ratings
+    global user_and_community_differences
 
-    if page == 0:
-        user_and_mutual_ratings = letterboxd_scraper.get_user_and_mutuals_ratings(username=username)
-        user_and_mutual_differences = ratings_analytics.get_differences_in_ratings(user_and_mutual_ratings)
-        for row_number in range(5):
-            star_rating = ""
-            for star in range(floor(user_and_mutual_differences.iloc[row_number]["Mutual Rating"])):
-                star_rating = star_rating + "★"
-            star_rating = star_rating if (floor(user_and_mutual_differences.iloc[row_number]["Mutual Rating"]) ==
-                                        user_and_mutual_differences.iloc[row_number]["Mutual Rating"]) else star_rating + "½"
-            user_and_mutual_differences.iat[row_number, 3] = star_rating
-            star_rating = ""
-            for star in range(floor(user_and_mutual_differences.iloc[row_number]["User Rating"])):
-                star_rating = star_rating + "★"
-            star_rating = star_rating if (floor(user_and_mutual_differences.iloc[row_number]["User Rating"]) ==
-                                        user_and_mutual_differences.iloc[row_number]["User Rating"]) else star_rating + "½"
-            user_and_mutual_differences.iat[row_number, 4] = star_rating
+    user_and_mutual_ratings = letterboxd_scraper.get_user_and_mutuals_ratings(username=username)
+    user_and_mutual_differences = ratings_analytics.get_differences_in_ratings(user_and_mutual_ratings)
+    for row_number in range(5):
+        star_rating = ""
+        for star in range(floor(user_and_mutual_differences.iloc[row_number]["Mutual Rating"])):
+            star_rating = star_rating + "★"
+        star_rating = star_rating if (floor(user_and_mutual_differences.iloc[row_number]["Mutual Rating"]) ==
+                                    user_and_mutual_differences.iloc[row_number]["Mutual Rating"]) else star_rating + "½"
+        user_and_mutual_differences.iat[row_number, 3] = star_rating
+        star_rating = ""
+        for star in range(floor(user_and_mutual_differences.iloc[row_number]["User Rating"])):
+            star_rating = star_rating + "★"
+        star_rating = star_rating if (floor(user_and_mutual_differences.iloc[row_number]["User Rating"]) ==
+                                    user_and_mutual_differences.iloc[row_number]["User Rating"]) else star_rating + "½"
+        user_and_mutual_differences.iat[row_number, 4] = star_rating
+
+    user_and_mutual_differences_poster_urls = tmdb_api_handler.get_film_posters(
+        user_and_mutual_differences["Film"][0:5])
+
+    # Putting posters in correct order
+    current_user_and_mutual_differences_poster_urls = [user_and_mutual_differences_poster_urls[film]
+                                 for film in user_and_mutual_differences["Film"].iloc[0:5]]
+
+    user_and_community_ratings = letterboxd_scraper.get_user_and_community_ratings(username=username)
+    user_and_community_differences = ratings_analytics.get_differences_in_ratings(user_and_community_ratings)
+
+    user_and_community_differences_poster_urls = tmdb_api_handler.get_film_posters(
+        user_and_community_differences["Film"][0:5])
+    
+    # Putting posters in correct order
+    current_user_and_community_differences_poster_urls = [user_and_community_differences_poster_urls[film]
+                                 for film in user_and_community_differences["Film"].iloc[0:5]]
+    
+    return templates.TemplateResponse(
+        "initial_mutual_and_community_results.html",
+        {
+            "request": request, 
+            "username": username,
+            "user_and_mutual_differences": user_and_mutual_differences.iloc[0:5],
+            "user_and_mutual_differences_poster_urls": current_user_and_mutual_differences_poster_urls,
+            "user_and_community_differences": user_and_community_differences.iloc[0:5],
+            "user_and_community_differences_poster_urls": current_user_and_community_differences_poster_urls
+        }
+        )
+
+@app.get('/additional-mutuals-differences/', response_class=HTMLResponse)
+def get_additional_mutuals_differences(request: Request, username: str, page: int = 0):
     
     user_and_mutual_differences_poster_urls = tmdb_api_handler.get_film_posters(
         user_and_mutual_differences["Film"][page*5:page*5+5])
@@ -48,19 +81,6 @@ def get_top_five_mutuals_differences(request: Request, username: str, page: int 
     current_page_film_posters = [user_and_mutual_differences_poster_urls[film]
                                  for film in user_and_mutual_differences["Film"].iloc[page*5:page*5+5]]
 
-    # If API has already been called than table for storing results has been created, and results but be
-    # table rows only
-    if page == 0:
-        return templates.TemplateResponse(
-        "mutuals_results.html",
-        {
-            "request": request, 
-            "username": username,
-            "page": page + 1,
-            "user_and_mutual_differences": user_and_mutual_differences.iloc[page*5:page*5+5],
-            "user_and_mutual_differences_poster_urls": current_page_film_posters
-        }
-        )
     return templates.TemplateResponse(
         "mutuals_results_additional_rows.html",
         {
@@ -72,17 +92,8 @@ def get_top_five_mutuals_differences(request: Request, username: str, page: int 
         }
         )
 
-@app.get('/top-five-community-differences/', response_class=HTMLResponse)
-def get_top_five_community_differences(request: Request, username: str, page: int = 0): 
-    
-    # API is called multiple times during infinite scroll so global variable are used to store values
-    global user_and_community_ratings
-    global user_and_community_differences
-
-    #TODO: convert user ratings to star
-    if page == 0:
-        user_and_community_ratings = letterboxd_scraper.get_user_and_community_ratings(username=username)
-        user_and_community_differences = ratings_analytics.get_differences_in_ratings(user_and_community_ratings)
+@app.get('/additional-community-differences/', response_class=HTMLResponse)
+def get_additional_community_differences(request: Request, username: str, page: int = 0): 
     
     user_and_community_differences_poster_urls = tmdb_api_handler.get_film_posters(
         user_and_community_differences["Film"][page*5:page*5+5])
@@ -90,19 +101,6 @@ def get_top_five_community_differences(request: Request, username: str, page: in
     current_page_film_posters = [user_and_community_differences_poster_urls[film]
                                 for film in user_and_community_differences["Film"].iloc[page*5:page*5+5]]
 
-    # If API has already been called than table for storing results has been created, and results but be
-    # table rows only
-    if page == 0:
-        return templates.TemplateResponse(
-            "community_results.html",
-            {
-                "request": request,
-                "username": username,
-                "page": page + 1,
-                "user_and_community_differences": user_and_community_differences.iloc[page*5:page*5+5],
-                "user_and_community_differences_poster_urls": current_page_film_posters
-            }
-        )
     return templates.TemplateResponse(
             "community_results_additional_rows.html",
             {
--- test_app/test_app.py
+++ test_app/test_app.py
@@ -7,10 +7,11 @@ from expected_responses.expected_top_five_differences_endpoint_response import E
 client = TestClient(app)
 
 def test_get_top_five_differences_endpoint():
-    response = client.get("/top-five-mutuals-differences", params={"username": "prowe"})
+    response = client.get("/initial-mutual-and-community-differences", params={"username": "prowe"})
     # This will break if someone posts a top five terrible take. 
     # Hopefully that's a rare occurrence.
     assert response.text == EXPECTED_TOP_FIVE_DIFFERENCES_ENDPOINT_RESPONSE
 
-def test_get_top_five_community_differences():
-    response = client.get("/top-five-community-differences", params={"username": "prowe"})
+def test_additional_community_differences():
+    response = client.get("/initial-mutual-and-community-differences", params={"username": "prowe"})
+    response = client.get("/additional-community-differences", params={"username": "prowe", "page": "1"})
